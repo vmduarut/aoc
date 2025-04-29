8f91794ea810b3922ccdf2f5b6a02a63a4ca1d4a
Victor Manuel Duarte Rute <victor.duarte@universaldx.com>
2025-04-29 12:04:47 +0200

    day6 part 2

--- 2024/day6/src/main.rs
+++ 2024/day6/src/main.rs
@@ -5,7 +5,7 @@ use std::path::Path;
 fn main() {
     let input = Path::new("./input.txt");
     let file_content = fs::read_to_string(input).expect("Error reading file content");
-    let matrix: Vec<_> = file_content
+    let mut matrix: Vec<_> = file_content
         .lines()
         .map(|line| line.trim().chars().collect::<Vec<_>>())
         .collect();
@@ -82,5 +82,83 @@ fn main() {
             }
         }
     }
-    println!("{}", distinct_positions.len());
+    println!("Distinct positions: {}", distinct_positions.len());
+
+    // Part 2
+    let mut positions_loop = HashSet::new();
+    for (x, y) in distinct_positions {
+        let original = matrix[x][y];
+        matrix[x][y] = '#';
+        if is_loop(&matrix, initial_guard) {
+            positions_loop.insert((x, y));
+        }
+        matrix[x][y] = original;
+    }
+
+    println!("Total obstructs: {:?}", positions_loop.len());
+}
+
+fn is_loop(matrix: &Vec<Vec<char>>, initial_guard: Option<(usize, usize)>) -> bool {
+    let mut direction = "up";
+    let mut visited: HashSet<(usize, usize, String)> = HashSet::new();
+    if let Some((mut i, mut j)) = initial_guard {
+        loop {
+            if visited.contains(&(i, j, direction.to_string())) {
+                return true;
+            }
+            if (i, j) != initial_guard.expect("No") {
+                visited.insert((i, j, direction.to_string()));
+            }
+            if i == matrix.len() || j == matrix[i].len() {
+                return false;
+            }
+
+            match direction {
+                "up" => {
+                    if matrix[i][j] == '#' {
+                        direction = "right";
+                        i += 1;
+                    } else {
+                        if i == 0 {
+                            return false;
+                        }
+                        i -= 1;
+                    }
+                }
+                "right" => {
+                    if matrix[i][j] == '#' {
+                        direction = "down";
+                        j -= 1;
+                    } else {
+                        j += 1;
+                    }
+                }
+                "down" => {
+                    if matrix[i][j] == '#' {
+                        direction = "left";
+                        i -= 1;
+                    } else {
+                        i += 1;
+                    }
+                }
+                "left" => {
+                    if matrix[i][j] == '#' {
+                        direction = "up";
+                        j += 1;
+                    } else {
+                        if j == 0 {
+                            return false;
+                        }
+                        j -= 1;
+                    }
+                }
+                _ => {
+                    println!("Nothing");
+                    return false;
+                }
+            }
+        }
+    } else {
+        return false;
+    }
 }
